356633bfc4b28e56d18c95ab1d0f670e5e48f414
Abhinav Singh <08abhinav03singh@gmail.com>
2025-08-12 18:05:16 +0530

    Added Day 19

---
+++ TeachersDay2025/Day19.java
@@ -0,0 +1,12 @@
+package TeachersDay2025;
+import java.util.Scanner;
+public class Day19 {
+    public static void main(String args[]){
+        Scanner scn = new Scanner(System.in);
+        int m = scn.nextInt();
+        int n = scn.nextInt();
+        int a= (m*n)/2;
+        System.out.println(a);
+        scn.close();
+    }
+}
